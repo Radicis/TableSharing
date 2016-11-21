884e37b705752e17161f105edcefabdf52ee01a1
Adam Lloyd <Adam Lloyd>
2016-11-21 17:44:38 +0000

    customised calendar view

--- client/modules/timetables/controllers/timetables.js
+++ client/modules/timetables/controllers/timetables.js
@@ -4,73 +4,12 @@ timetableModule.controller('TimetableController', function($scope, $routeParams,
     var m = date.getMonth();
     var y = date.getFullYear();
 
-    /* event source that pulls from google.com */
-    $scope.eventSource = {
-        url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
-        className: 'gcal-event',           // an option!
-        currentTimezone: 'America/Chicago' // an option!
-    };
-
-    /* event source that contains custom events on the scope */
-    $scope.events = [
-        {title: 'All Day Event',start: new Date(y, m, 1)},
-        {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-        {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-        {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-        {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-        {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-    ];
-
-    /* event source that calls a function on every view switch */
-    $scope.eventsF = function (start, end, timezone, callback) {
-        var s = new Date(start).getTime() / 1000;
-        var e = new Date(end).getTime() / 1000;
-        var m = new Date(start).getMonth();
-        var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
-        callback(events);
-    };
-
-    $scope.calEventsExt = {
-        color: '#f00',
-        textColor: 'yellow',
-        events: [
-            {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-            {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-            {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
-    };
-
-    /* alert on eventClick */
-    $scope.alertOnEventClick = function( date, jsEvent, view){
-        console.log("helo event click");
-        $scope.alertMessage = (date.title + ' was clicked ');
-    };
-    /* alert on Drop */
-    $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
-        $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
-    };
-    /* alert on Resize */
-    $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
-        $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
-    };
-
-    /* event sources array*/
-    $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
-    $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
-    /* add custom event*/
-    $scope.addEvent = function() {
-        $scope.events.push({
-            title: 'Open Sesame',
-            start: new Date(y, m, 28),
-            end: new Date(y, m, 29),
-            className: ['openSesame']
-        });
-    };
+    $scope.eventSources = [];
 
     $scope.getAllTables = function(){
-      TimetableService.getAll().then(function(tables){
-        $scope.tables = tables;
-      });
+        TimetableService.getAll().then(function(tables){
+            $scope.tables = tables;
+        });
     };
 
     $scope.getTable = function(){
@@ -79,12 +18,9 @@ timetableModule.controller('TimetableController', function($scope, $routeParams,
         TimetableService.getTableById(id).then(function(table){
             $scope.table = table;
             $scope.getEventsByTableId(id);
-
+            console.log($scope.events);
+            $scope.initTable();
         });
-    };
-
-    $scope.message = function() {
-        $scope.dp.message("Hi");
     };
 
     $scope.getEventsByTableId = function(id){
@@ -94,32 +30,67 @@ timetableModule.controller('TimetableController', function($scope, $routeParams,
         // });
     };
 
-    $scope.addEvent = function () {
-        // $scope.events.push(
-        //     {
-        //         start: new DayPilot.Date("2016-11-14T10:00:00"),
-        //         end: new DayPilot.Date("2016-11-14T12:00:00"),
-        //         id: DayPilot.guid(),
-        //         text: "Simple Event 2"
-        //     }
-        // );
-        // console.log("Events: " + $scope.events.length);
-        // $scope.message();
+    $scope.initTable = function(){
+        $scope.personalEvents = {
+            color: '#f00',
+            textColor: 'yellow',
+            events: [
+                {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+                {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+                {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+            ]
+        };
+
+        /* event sources array*/
+        $scope.eventSources.push($scope.personalEvents);
+        $scope.eventSources.push($scope.events);
+
+        // Calendar configuration
+        $scope.uiConfig = {
+            calendar:{
+                columnFormat: 'ddd M/D',
+                height: 442,
+                slotDuration: '00:30:00',
+                editable: true,
+                defaultView: "agendaWeek",
+                nowIndicator: true,
+                allDaySlot: false,
+                minTime: '09:00:00',
+                maxTime: '18:00:00',
+                scrollTime: '09:00:00',
+                firstDay: 1,
+                allDayText: '',
+                hiddenDays: [0, 6],
+                header:{
+                    left: 'agendaWeek, month',
+                    center: '',
+                    right: 'today prev,next'
+                },
+
+                eventClick: $scope.editEvent,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize
+            }
+        };
+
     };
 
-    $scope.uiConfig = {
-        calendar:{
-            height: 450,
-            editable: true,
-            header:{
-                left: 'agendaWeek',
-                center: 'title',
-                right: 'today prev,next'
-            },
-            eventClick: $scope.alertOnEventClick,
-            eventDrop: $scope.alertOnDrop,
-            eventResize: $scope.alertOnResize
-        }
+    /* alert on Drop */
+    $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
+        $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
+    };
+    /* alert on Resize */
+    $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
+        $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+    };
+
+
+    $scope.addEvent = function() {
+        EventService.addEventModal();
+    };
+
+    $scope.editEvent = function() {
+        EventService.editEventModal();
     };
 
 });
--- client/modules/timetables/events/services/events.js
+++ client/modules/timetables/events/services/events.js
@@ -1,5 +1,33 @@
 eventsModule.service('EventService', function($http, $q, $uibModal) {
 
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    this.getEventsByTableId = function (tableId) {
+        // var def = $q.defer();
+        // $http.get('/api/events/' + tableId).success(function (response) {
+        //     def.resolve(response);
+        // }).error(function (error) {
+        //     console.log("Error: " + error);
+        //     def.reject(null);
+        // });
+        // return def.promise;
+        //return events;
+        /* event source that contains custom events on the scope */
+        var events = [
+            {title: 'All Day Event',start: new Date(y, m, 1)},
+            {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+            {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+            {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+            {title: 'Birthday Party',start: new Date(y, m, d + 1, 10, 0),end: new Date(y, m, d + 1, 10, 30),allDay: false}
+        ];
+
+        return events;
+
+    };
+
     this.editEventModal = function (event) {
 
         console.log("Opening event edit modal");
@@ -23,18 +51,29 @@ eventsModule.service('EventService', function($http, $q, $uibModal) {
         });
     };
 
-    this.getEventsByTableId = function (tableId) {
-        // var def = $q.defer();
-        // $http.get('/api/events/' + tableId).success(function (response) {
-        //     def.resolve(response);
-        // }).error(function (error) {
-        //     console.log("Error: " + error);
-        //     def.reject(null);
-        // });
-        // return def.promise;
-        //return events;
+    this.addEventModal = function (event) {
 
+        console.log("Opening event add modal");
+
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: '/modules/timetables/events/views/edit.html',
+            controller: 'EventController',
+            size: 100,
+            resolve: {
+                items: function () {
+                    return null;
+                }
+            }
+        });
+
+        modalInstance.result.then(function () {
+            console.log("Modal did stuff");
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
     };
+
 
     this.addEvent = function(event){
 
